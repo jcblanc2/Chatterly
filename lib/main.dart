2bf2dad53f440574ae91aa5b5404ce037a456b94
jcblanc2 <jclaytonblanc@gmail.com>
2023-08-10 12:34:23 -0400

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:chatterly/services/auth.dart';
+import 'package:chatterly/firebase_options.dart';
+import 'package:chatterly/services/auth/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
